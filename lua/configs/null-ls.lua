cac3e1545a1bdcd23c0fae707cd4f3277e4a9d34
Gustavo T. Rudiger <gustavo@trudax.tech>
2023-06-20 17:39:53 -0300

    feat: 🎸 add go formatters

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -3,29 +3,32 @@ local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local on_attach = function(client, bufnr)
-	if client.supports_method("textDocument/formatting") then
-		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = augroup,
-			buffer = bufnr,
-			callback = function()
-				vim.lsp.buf.format()
-			end,
-		})
-	end
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ bufnr = bufnr })
+      end,
+    })
+  end
 end
 
 null_ls.setup({
-	debug = true,
-	sources = {
-		null_ls.builtins.code_actions.eslint_d,
-		null_ls.builtins.formatting.prettierd,
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.diagnostics.codespell,
-		null_ls.builtins.diagnostics.eslint_d,
-		null_ls.builtins.diagnostics.jsonlint,
-		null_ls.builtins.diagnostics.markdownlint,
-		null_ls.builtins.diagnostics.yamllint,
-	},
-	on_attach = on_attach,
+  debug = true,
+  sources = {
+    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.gofumpt,
+    null_ls.builtins.formatting.goimports_reviser,
+    null_ls.builtins.formatting.golines,
+    null_ls.builtins.diagnostics.codespell,
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.diagnostics.jsonlint,
+    null_ls.builtins.diagnostics.markdownlint,
+    null_ls.builtins.diagnostics.yamllint,
+  },
+  on_attach = on_attach,
 })
