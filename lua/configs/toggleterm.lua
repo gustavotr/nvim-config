ffc25abdef55b739889a6e97306efc87d2eccd25
Gustavo T. Rudiger <gustavo@trudax.tech>
2023-06-22 23:39:27 -0300

    fix: 🐛 vertical term size is too small

--- lua/configs/toggleterm.lua
+++ lua/configs/toggleterm.lua
@@ -4,19 +4,19 @@ if not status_ok then
 end
 
 toggleterm.setup({
-	size = 20,
-	open_mapping = [[<c-\>]],
-	hide_numbers = true,
-	shade_terminals = true,
-	shading_factor = 2,
-	start_in_insert = true,
-	insert_mappings = true,
+	size = function(term)
+		if term.direction == "horizontal" then
+			return 15
+		elseif term.direction == "vertical" then
+			return vim.o.columns * 0.4
+		end
+	end,
 	persist_size = true,
 	direction = "horizontal",
 	close_on_exit = true,
 	shell = "/bin/zsh",
 	float_opts = {
-		border = "curved",
+		border = "single",
 	},
 })
 
