2b0d4a58d0b3eb1214e782d6eb4946809495eb1d
Gustavo T. Rudiger <gustavo@trudax.tech>
2023-07-07 22:36:57 -0300

    feat: 🎸 add keymap for navigation quick list

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -38,14 +38,18 @@ keymap("n", "<C-Right>", "<cmd>vertical resize +2<CR>", opts)
 keymap("n", "<S-l>", "<cmd>bnext<CR>", opts)
 keymap("n", "<S-h>", "<cmd>bprevious<CR>", opts)
 
+-- Navigate Quick list
+keymap("n", "]q", "<cmd>cnext<CR>", opts)
+keymap("n", "[q", "<cmd>cprevious<CR>", opts)
+
 -- Clear highlights
 keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", { silent = true, desc = "Clear Highlights" })
 
 -- Close buffers
 keymap("n", "<leader>c", "<cmd>Bdelete!<CR>", { silent = true, desc = "Close" })
 keymap("n", "<leader>bo", function()
-  vim.cmd.BufferLineCloseLeft()
-  vim.cmd.BufferLineCloseRight()
+	vim.cmd.BufferLineCloseLeft()
+	vim.cmd.BufferLineCloseRight()
 end, { silent = true, desc = "Close others" })
 keymap("n", "<leader>bl", "<cmd>BufferLineCloseRight<CR>", { silent = true, desc = "Close right" })
 keymap("n", "<leader>bh", "<cmd>BufferLineCloseLeft<CR>", { silent = true, desc = "Close left" })
@@ -82,10 +86,10 @@ keymap("n", "<leader>gvr", "<cmd>DiffviewRefresh<cr>", { desc = "Diffview Refres
 -- Debug
 keymap("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", { silent = true, desc = "Breakpoint" })
 keymap(
-  "n",
-  "<Leader>dB",
-  "<Cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
-  { silent = true, desc = "Breakpoint condition" }
+	"n",
+	"<Leader>dB",
+	"<Cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+	{ silent = true, desc = "Breakpoint condition" }
 )
 keymap("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>", { silent = true, desc = "Continue" })
 keymap("n", "<leader>di", "<cmd>lua require'dap'.step_into()<cr>", { silent = true, desc = "Step Into" })
@@ -97,19 +101,19 @@ keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", { silent = tru
 keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", { silent = true, desc = "Terminate" })
 keymap("n", "<leader>dh", "<cmd>lua require'dap.ui.widgets'.hover()<CR>")
 keymap(
-  "n",
-  "<Leader>dl",
-  "<Cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
-  { silent = true, desc = "Breakpoint log" }
+	"n",
+	"<Leader>dl",
+	"<Cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
+	{ silent = true, desc = "Breakpoint log" }
 )
 keymap("n", "<Leader>dL", "<Cmd>lua require'dap'.run_last()<CR>", { silent = true, desc = "Run last" })
 
 -- Toggle
 keymap(
-  "n",
-  "<leader>tt",
-  "<cmd>TroubleToggle document_diagnostics<cr>",
-  { silent = true, noremap = true, desc = "Toggle Trouble Doc" }
+	"n",
+	"<leader>tt",
+	"<cmd>TroubleToggle document_diagnostics<cr>",
+	{ silent = true, noremap = true, desc = "Toggle Trouble Doc" }
 )
 keymap("n", "<leader>tT", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true, desc = "Toggle Trouble" })
 keymap({ "n", "t" }, "<A-1>", "<cmd>ToggleTerm direction=horizontal<cr>")
